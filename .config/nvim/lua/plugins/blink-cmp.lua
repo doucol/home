c8ffeb9aac4400da98953746ac17acc1a27193e8
Doug Collier <540588+doucol@users.noreply.github.com>
2025-02-24 08:47:37 -0600

    minor updates

--- .config/nvim/lua/plugins/blink-cmp.lua
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -2,18 +2,18 @@ return {
   "saghen/blink.cmp",
   enabled = true,
   opts = function(_, opts)
-    opts.sources = vim.tbl_deep_extend("force", opts.sources or {}, {
-      cmdline = function()
-        local type = vim.fn.getcmdtype()
-        if type == "/" or type == "?" then
-          return { "buffer" }
-        end
-        if type == ":" then
-          return { "cmdline" }
-        end
-        return {}
-      end,
-    })
+    -- opts.sources = vim.tbl_deep_extend("force", opts.sources or {}, {
+    --   cmdline = function()
+    --     local type = vim.fn.getcmdtype()
+    --     if type == "/" or type == "?" then
+    --       return { "buffer" }
+    --     end
+    --     if type == ":" then
+    --       return { "cmdline" }
+    --     end
+    --     return {}
+    --   end,
+    -- })
 
     opts.completion = { list = { selection = { preselect = false } } }
     opts.keymap = {
